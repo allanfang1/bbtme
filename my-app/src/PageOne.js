8e50b047d8c26f33098c5eff4e7cab115b5aa44c
Allan Fang <32990185+allanfang1@users.noreply.github.com>
2022-11-18 20:16:39 -0500

    some restructuring, began doing the slider with the cup

--- my-app/src/PageOne.js
+++ my-app/src/PageOne.js
@@ -10,37 +10,66 @@ export default function PageOne(){
         {name:"Tuning", imglink:"/media/allan.png", content:["Sugar", "Ice"]},
         {name:"Love", imglink:"/media/allan.png", content:["Yes"]},
     ]
-    const [current, setCurrent] = useState({cat: "", content: [], opt:""});
+    const [currCat, setCurrCat] = useState({name: "", content: []});
+    const [currOpt, setCurrOpt] = useState("");
     const [orders, setOrder] = useState([]);
     const [drink, setDrink] = useState({cap:100, contains:{}, sugar: 100, id: 0});
+    const [currNum, setCurrNum] = useState(0);
 
     useEffect(() => {
-        console.log(current);
-        console.log(orders);
+        // console.log(currCat);
+        // console.log(orders);
         // console.log(JSON.stringify(orders[orders.length -1]));
-    }, [current, orders]);
+    }, [currCat, orders]);
 
-    function addElement(name, percent){
-        if (name === "Sugar" && Number(percent) < 200){
-            setDrink({cap: drink.cap, contains: drink.contains, sugar: percent});
+    useEffect(() => {
+        if (!drink.contains[currOpt]){
+            setCurrNum(0);
+        } else {
+            setCurrNum(drink.contains[currOpt]);
+        }
+    }, [currOpt]);
+
+    useEffect(() => {
+        setCurrNum(0);
+        setCurrOpt("");
+    }, [currCat]);
+
+    function padSetNum(x){
+        if (!currOpt){
+            return;
+        } else if (x === -1 && currNum<10){
+            setCurrNum(0);
+        } else if (x === -1){
+            setCurrNum(Number(String(currNum).substring(0, String(currNum).length - 1)));
+        } else if (currNum === 0){
+            setCurrNum(x);
+        } else if (Number("" + currNum + x) < drink.cap){
+            setCurrNum(Number("" + currNum + x));
+        }
+    }
+
+    function addElement(name){
+        if (name === "Sugar" && Number(currNum) < 200){
+            setDrink({cap: drink.cap, contains: drink.contains, sugar: currNum, id: drink.id});
             return;
         }
         if (!name){return;}
         let newCap;
         if (drink.contains.hasOwnProperty(name)){
-            newCap = drink.cap + Number(drink.contains[name]) - Number(percent);
+            newCap = drink.cap + drink.contains[name] - currNum;
         } else {
-            newCap = drink.cap - Number(percent);
+            newCap = drink.cap - currNum;
         }
         if (newCap < 0){
             console.log("Too much");
             return;
         }
         let tempContains = drink.contains;
-        if (Number(percent) === 0){
+        if (currNum === 0){
             delete tempContains[name];
         } else {
-            tempContains[name] = percent;
+            tempContains[name] = currNum;
         }
         setDrink({cap: newCap, contains: tempContains, sugar:100, id: drink.id});
         console.log(JSON.stringify(drink));
@@ -52,18 +81,18 @@ export default function PageOne(){
                 {categories.map((catObj, index) => (
                     <Category 
                         catObj = {catObj}
-                        currentCat = {current.cat}
-                        clickFunc = {setCurrent}
+                        currentCat = {currCat.name}
+                        clickFunc = {setCurrCat}
                         key = {index}
                     />
                 ))}
             </div>
             <div className="cols col2">
-                {current.content.map((optName, index) => (
+                {currCat.content.map((optName, index) => (
                     <Option 
                         optName = {optName}
-                        current = {current}
-                        clickFunc = {setCurrent}
+                        currOpt = {currOpt}
+                        clickFunc = {setCurrOpt}
                         key = {index}
                     />
                 ))}
@@ -71,9 +100,21 @@ export default function PageOne(){
             <div className="col3">   
                 <div className="add-button">{JSON.stringify(drink)}</div>
                 <div className="add-button">{JSON.stringify(orders)}</div>
+                <div className="add-button">
+                    <div className="drink-visual">
+                        {Object.keys(drink.contains).map((key, index) => (
+                            <svg width="100px" height={drink.contains[key]} key={index}>
+                                <rect width="100px" height={drink.contains[key]} fill="blue" />
+                            </svg>
+                        ))}
+                    </div>
+                </div>
+                <div className="add-button">
+                    <input type="range" id="points" name="points" min="0" max="100" value={currNum}/>
+                </div>
             </div>
             <div className="col4">
-                <Numpad optName={current.opt} val={drink.contains[current.opt]} addFunc={addElement}/>
+                <Numpad optName={currOpt} val={currNum} addFunc={addElement} setNumFunc={padSetNum}/>
                 <div 
                     className="add-button clickable noselect" 
                     onClick={()=>{setOrder([...orders, drink]); setDrink({cap:100, contains:{}, sugar: 100, id: drink.id + 1})}}>
--- my-app/src/components/Category.js
+++ my-app/src/components/Category.js
@@ -5,7 +5,7 @@ const Category = ({catObj, currentCat, clickFunc}) => {
         <div 
             className= "col-box noselect clickable"
             id={currentCat === catObj.name ? "selected" : "" } 
-            onClick={currentCat === catObj.name ? null : () => clickFunc({cat: catObj.name, content: catObj.content})} >
+            onClick={currentCat === catObj.name ? null : () => clickFunc({name: catObj.name, content: catObj.content})} >
                 <img className="cat-img" src={catObj.imglink} alt="pee pee"/>
                 <div className="cat-name">{catObj.name}</div>
         </div>
