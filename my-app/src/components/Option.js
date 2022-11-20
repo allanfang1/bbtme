2aa6050fafd24d614aedbdb4fb1a484b432f7236
sawyercole <sawyercoletang@gmail.com>
2022-11-20 16:14:00 -0500

    added some imaged and css to options

--- my-app/src/components/Option.js
+++ my-app/src/components/Option.js
@@ -1,11 +1,17 @@
 import { React } from "react"
 
+function changeBackground(e, color) {
+        e.target.style.background = color;
+}
+
 const Option = ({optName, currOpt, clickFunc}) => {
     return (
         <div 
             className="col-box noselect clickable"
             id={currOpt === optName ? "selected" : "" } 
-            onClick={() => clickFunc(optName)} >
+            onClick={() => clickFunc(optName)} 
+            onMouseEnter={(e) => changeBackground(e, "#faedcd")}
+            onMouseLeave={(e) => changeBackground(e, "#fefae0")}>
                 <div className="cat-name">{optName}</div>
                 {/* <div>{current}</div> */}
         </div>
