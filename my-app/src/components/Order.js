3c392290b58ea2982df1f02d185655e2fc13c9cc
Brian Le <briantule3@gmail.com>
2022-11-20 22:05:24 -0500

    Style added

--- my-app/src/components/Order.js
+++ my-app/src/components/Order.js
@@ -1,8 +1,14 @@
-import { React } from "react"
+import { React, useState } from "react"
 
 const Order = ({ ordObj, key }) => {
+    const [hidden, setHidden] = useState("");
+    function hideDiv() {
+        //var x = document.getElementsByClassName("order-cell");
+        // document.getElementById('hideDiv').style.display='none';
+        setHidden("hideDiv");
+    }
     return (
-        <div className="order-cell">
+        <div className="order-cell" id={hidden}>
             <div className="order-cell">{"Order ID: " + ordObj.orderID}</div>
             <div className="order-cell">{ordObj.time}</div>
             <br></br>
@@ -10,7 +16,10 @@ const Order = ({ ordObj, key }) => {
             <div>{ordObj.base}</div>
             <div>{ordObj.topping[0]} {ordObj.topping[1] + "%"}</div>
             <div>{"Sugar: " + ordObj.sugar + "%"}</div>
-            <div>{"Ice: " + ordObj.ice+"%"}</div>
+            <div>{"Ice: " + ordObj.ice + "%"}</div>
+            <div onClick={hideDiv} className="resolve-cell">
+            RESOLVE
+            </div>
         </div>
     );
 }
